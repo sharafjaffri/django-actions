dd0577cfae3057b670478c9fed762dea71bf32fd
Sharaf Ali <sharafjaffri@live.com>
2014-07-12 13:58:09 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ import os
 CLASSIFIERS = []
 
 setup(
-    author="QDQ media",
-    author_email="dpto-tecnico@qdqmedia.com",
+    author="Sharaf Jaffri",
+    author_email="sharaf@bitswits.com",
     name='django-actions',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    url='https://github.com/qdqmedia/django-actions/',
+    url='https://github.com/sharafjaffri/django-actions/',
     license='BSD License',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
